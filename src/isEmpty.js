05e79ff239420acd541f603f34d8602f6a043174
starkwang <wsd312@163.com>
2019-12-21 20:58:59 +0800

    feat: init

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -6,18 +6,45 @@
  * @LastEditTime : 2019-12-21 16:00:02
  * @Description: file content
  */
+const NODE_ENV = process.env.NODE_ENV;
+const invariant = function(condition, format, a, b, c, d, e, f) {
+  if (NODE_ENV !== 'production') {
+    if (format === undefined) {
+      throw new Error('invariant requires an error message argument');
+    }
+  }
 
-'use strict';
-// const invariant = require('invariant');
+  if (!condition) {
+    var error;
+    if (format === undefined) {
+      error = new Error(
+        'Minified exception occurred; use the non-minified dev environment ' +
+        'for the full error message and additional helpful warnings.'
+      );
+    } else {
+      var args = [a, b, c, d, e, f];
+      var argIndex = 0;
+      error = new Error(
+        format.replace(/%s/g, function() { return args[argIndex++]; })
+      );
+      error.name = 'Invariant Violation';
+    }
+
+    error.framesToPop = 1;
+    throw error;
+  }
+};
+
+
 export const isEmpty = (value) => {
   if (Array.isArray(value)) {
     return value.length === 0;
   } else if (typeof value === 'object') {
     if (value) {
-      // invariant(
-      //   !isIterable(value) || value.size === undefined,
-      //   'isEmpty() does not support iterable collections.',
-      // );
+      invariant(
+        !isIterable(value) || value.size === undefined,
+        'isEmpty() does not support iterable collections.',
+      );
       for (const _ in value) {
         return false;
       }
